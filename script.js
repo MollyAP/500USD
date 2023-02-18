8a4ea2ec120b53bc92f2c4ef1132ef7e70cfbd79
Mollyphobia <123973226+Mollyphobia@users.noreply.github.com>
2023-02-17 19:46:44 -0600

    fixed but had to get rid of my popups

--- script.js
+++ script.js
@@ -1,14 +1,6 @@
-window.open('https://cash.app/$mopmlhca/500.00','popupad','width=auto,height=auto');
-window.open('https://cash.app/$mopmlhca/500.00','popupad','width=auto,height=auto');
-window.open('https://cash.app/$mopmlhca/500.00','popupad','width=auto,height=auto');
-window.open('https://cash.app/$mopmlhca/500.00','popupad','width=auto,height=auto');
-window.open('https://cash.app/$mopmlhca/500.00','popupad','width=auto,height=auto');
-window.open('https://cash.app/$mopmlhca/500.00','popupad','width=auto,height=auto');
-window.open('https://cash.app/$mopmlhca/500.00','popupad','width=auto,height=auto');
-window.open('https://cash.app/$mopmlhca/500.00','popupad','width=auto,height=auto');
-
-
-if (window.confirm("Do you want to give me $500 USD?")) {
+setTimeout(function() { 
+    if (window.confirm("Do you want to give me $500 USD?")) {
     window.open("https://cash.app/$mopmlhca/500.00");
 }
-else window.location.assign("sadkuromi.html");
+else window.location.assign("sadkuromi.html");  }, 1000);
+
