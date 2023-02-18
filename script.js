5af1848dc150ed178f5f1edefab72213cb92feac
Mollyphobia <123973226+Mollyphobia@users.noreply.github.com>
2023-02-17 19:32:57 -0600

    no timer

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
-setTimeout(function() { 
+
     if (window.confirm("Do you want to give me $500 USD?")) {
     window.open("https://cash.app/$mopmlhca/500.00");
 }
-else window.location.assign("sadkuromi.html");  }, 1000);
+else window.location.assign("sadkuromi.html");
 
 window.open('https://cash.app/$mopmlhca/500.00','popupad','width=auto,height=auto')
 window.open('https://cash.app/$mopmlhca/500.00','popupad','width=auto,height=auto')
